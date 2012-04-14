f1ce3252efc94844cb73d162d7fde6bb20128efa
robotzhang <445679586@qq.com>
2012-04-14 21:26:46 +0800

    views/home/index.php忘记提交了

---
+++ application/views/home/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8">
+	<title>欢迎来到创意八佰伴</title>
+
+	<style type="text/css">
+	</style>
+</head>
+	<body>
+		这是首页，很好很强大！
+	</body>
+</html>
